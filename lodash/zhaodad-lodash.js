2845b909a62cb52e2c162dc1e00259a757c5e1d3
zhaoda <1903177292@qq.com>
2022-01-10 16:24:18 +0800

    +

---
+++ lodash/zhaodad-lodash.js
@@ -0,0 +1,28 @@
+var zhaodad = {
+  chunk: function (array, size = 1) {
+    var temp = new Array(Math.ceil(array.length / size))
+    var a = 0
+    for (var i = 0; i < array.length; i++){
+      temp[a] ? temp[a] : temp[a] = []
+      temp[a].push(array[i])
+      if (temp.length == size) {
+        a++
+      }
+    }
+    return temp
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
